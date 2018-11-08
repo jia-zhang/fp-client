99db38e21f5a9cf62048f073c52fcaea78423513
root <root@nj-jia-zhang.client.tw.trendnet.org>
2018-11-08 17:45:21 +0800

    update

--- lib/stock_mailer.py
+++ lib/stock_mailer.py
@@ -14,6 +14,8 @@ class StockMailer():
         #self.rcpt_list = "286531599@qq.com,jenixe@126.com"
         self.rcpt_list = ["jenixe@126.com","286531599@qq.com","jenixg@gmail.com"]
         self.util = StockUtil()
+        self.mail_server = '127.0.0.1'
+        self.mail_port = 25
         self.logger = Logger("StockMailer")
 
     def send_fp_mail(self,stock_list):
@@ -24,12 +26,30 @@ class StockMailer():
         self.logger.info(msg_body)
         for rcpt in self.rcpt_list:
             self.logger.info("Sending mail to %s"%(rcpt))
-            self.send_mail_to_one_rcpt(rcpt,msg_subject,msg_body)
+            #self.send_mail_to_one_rcpt(rcpt,msg_subject,msg_body)
+            self.send_mail_from_local_to_one_rcpt(rcpt,msg_subject,msg_body)
             time.sleep(5)
     
     def send_mail(self,rcpt_list):
         #for rcpt in rcpt_list:
         pass
+
+    def send_mail_from_local_to_one_rcpt(self,rcpt,msg_subject,msg_body):
+        ret=True
+        try:
+            msg=MIMEText(msg_body,'plain','utf-8')
+            msg['From']=formataddr(["jiazzz",self.my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+            msg['To']=formataddr(["whoareyou",rcpt])              # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+            msg['Subject']="%s-%s"%(msg_subject,self.date)                # 邮件的主题，也可以说是标题
+            server=smtplib.SMTP(self.mail_server,self.mail_port)  # 发件人邮箱中的SMTP服务器，端口是465
+            server.login(self.my_sender, self.my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
+            server.sendmail(self.my_sender,[rcpt,],msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+            server.quit()# 关闭连接
+        except Exception:# 如果 try 中的语句没有执行，则会执行下面的 ret=False
+            #print("aaabbb")
+            ret=False
+        return ret
+    
     
     def send_mail_to_one_rcpt(self,rcpt,msg_subject,msg_body):
         ret=True
