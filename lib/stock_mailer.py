62d9f1895f613d7181f3c6b73fd4e725e8bd70eb
root <root@nj-jia-zhang.client.tw.trendnet.org>
2018-11-08 17:33:13 +0800

    update

--- lib/stock_mailer.py
+++ lib/stock_mailer.py
@@ -4,25 +4,27 @@ from email.utils import formataddr
 import datetime
 from stock_util import StockUtil
 from logger import Logger
+import time
 
 class StockMailer():
     def __init__(self):
         self.my_sender='3100820192@qq.com'    # 发件人邮箱账号
         self.my_pass = 'hymygengdhnydhfd'              # 发件人邮箱密码(当时申请smtp给的口令)
-        self.date = datetime.datetime.now().strftime('%Y_%m_%d')
+        self.date = datetime.datetime.now().strftime('%Y/%m/%d')
         #self.rcpt_list = "286531599@qq.com,jenixe@126.com"
-        self.rcpt_list = ["jenixe@126.com"]
+        self.rcpt_list = ["jenixe@126.com","286531599@qq.com","jenixg@gmail.com"]
         self.util = StockUtil()
         self.logger = Logger("StockMailer")
 
     def send_fp_mail(self,stock_list):
         today = self.util.get_today()
-        msg_subject = "%s 复盘结果"%(today)
+        msg_subject = "复盘结果"
         stock_status = self.util.get_summary_status_after_close(stock_list)    
         msg_body = "\n".join(stock_status)
         self.logger.info(msg_body)
         for rcpt in self.rcpt_list:
             self.send_mail_to_one_rcpt(rcpt,msg_subject,msg_body)
+            time.sleep(5)
     
     def send_mail(self,rcpt_list):
         #for rcpt in rcpt_list:
@@ -40,7 +42,7 @@ class StockMailer():
             server.sendmail(self.my_sender,[rcpt,],msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
             server.quit()# 关闭连接
         except Exception:# 如果 try 中的语句没有执行，则会执行下面的 ret=False
-            print("aaabbb")
+            #print("aaabbb")
             ret=False
         return ret
 
